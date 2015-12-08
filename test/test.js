18be6f833f46cefedc9f7b77a45263fb4602613e
Paulo Avila <pauloavila@gmail.com>
2015-12-08 01:27:12 +0000

    Grouped tests, added real-world cases

--- test/test.js
+++ test/test.js
@@ -3,27 +3,38 @@
 var expect = require('chai').expect;
 var a8e = require('../index');
 
-describe('#numeronymize', function () {
-  it('should return a stringified version of input', function () {
-    var result = a8e(1);
-    expect(result).to.be.a('string');
-    expect(result).to.equal('1');
+describe('a8e', function() {
+  describe('#edgeCases', function () {
+    it('should return a stringified version of input', function () {
+      var result = a8e(1);
+      expect(result).to.be.a('string');
+      expect(result).to.equal('1');
+    });
+
+    it('should return same input if length less than 3', function () {
+      var input0 = '';
+      var input1 = 'I';
+      var input2 = 'yo';
+      var input3 = 'foo';
+
+      expect(a8e(input0)).to.equal(input0);
+      expect(a8e(input1)).to.equal(input1);
+      expect(a8e(input2)).to.equal(input2);
+      expect(a8e(input3)).to.not.equal(input3);
+    });
   });
 
-  it('should return same input if length less than 3', function () {
-    var input0 = '';
-    var input1 = 'I';
-    var input2 = 'yo';
-    var input3 = 'foo';
+  describe('#numeronymization', function () {
+    var tests = [
+      {input: 'localization',         output: 'l10n'},
+      {input: 'internationalization', output: 'i18n'}
+    ];
 
-    expect(a8e(input0)).to.equal(input0);
-    expect(a8e(input1)).to.equal(input1);
-    expect(a8e(input2)).to.equal(input2);
-    expect(a8e(input3)).to.not.equal(input3);
-  });
-
-  it('should return same input if length less than 3', function () {
-    var result = a8e('foo');
-    expect(result).to.equal('f1o');
+    tests.forEach(function (test) {
+      it('should work for <' + test.input + '>', function () {
+        var result = a8e(test.input);
+        expect(result).to.equal(test.output);
+      });
+    });
   });
 });
