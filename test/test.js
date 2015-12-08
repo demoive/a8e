fcc6d075d1d9f5e8db98fea28a5e60381cb6a13b
Paulo Avila <pauloavila@gmail.com>
2015-12-08 00:56:20 +0000

    More tests for input length

--- test/test.js
+++ test/test.js
@@ -4,14 +4,22 @@ var expect = require('chai').expect;
 var a8e = require('../index');
 
 describe('#numeronymize', function () {
-  it('should return stringified version of input', function () {
+  it('should return a stringified version of input', function () {
     var result = a8e(1);
+    expect(result).to.be.a('string');
     expect(result).to.equal('1');
   });
 
   it('should return same input if length less than 3', function () {
-    var result = a8e('hi');
-    expect(result).to.equal('hi');
+    var input0 = '';
+    var input1 = 'I';
+    var input2 = 'yo';
+    var input3 = 'foo';
+
+    expect(a8e(input0)).to.equal(input0);
+    expect(a8e(input1)).to.equal(input1);
+    expect(a8e(input2)).to.equal(input2);
+    expect(a8e(input3)).to.not.equal(input3);
   });
 
   it('should return same input if length less than 3', function () {
